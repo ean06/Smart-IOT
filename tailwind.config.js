f057f23dcd8b6389b02b266e6692a2b282fe3ee7
ean06 <raihanfadilah0406@gmail.com>
2023-09-08 09:53:13 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {
+      fontFamily:{
+          
+      },
+      colors : {
+        "hijau" : '#00A868',
+        "hover" : '#038c58',
+        "biru" : '#192736',
+        "abu" : '#D9D9D9',
+        "bg" : '#A3F7BF'
+      }
+  },
+  plugins: [],
+}
+}
